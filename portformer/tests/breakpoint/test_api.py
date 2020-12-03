42af4a189ad6096e307a1062b48743d2b5c7fc66
Sean Kruzel <skruzel@astrocyte.io>
2020-12-03 04:09:03 -0500

    improved error code

--- portformer/tests/breakpoint/test_api.py
+++ portformer/tests/breakpoint/test_api.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from portformer import BreakpointAPI
+from portformer.errors import PortformerInvalidAPIKeyError, PortformerMissingAPIKeyError
 
 
 class TestBreakpointAPI(unittest.TestCase):
@@ -13,3 +14,21 @@ class TestBreakpointAPI(unittest.TestCase):
 
         api = BreakpointAPI(api_key="TEST")
         self.assertEqual(api.api_key, "TEST")
+
+    def test_requires_auth_key(self):
+        """Should return 401 / 403 Not authenticated if given a bad or missing api_keys"""
+
+        api = BreakpointAPI(api_key="NOT-A-VALID-KEY")
+        self.assertRaises(
+            PortformerInvalidAPIKeyError, api.forecast, ("TSLA",),
+        )
+
+        api = BreakpointAPI(api_key=None)
+        api.api_key = None  # force non in case environ variable is set
+        self.assertRaises(
+            PortformerMissingAPIKeyError, api.forecast, ("TSLA",),
+        )
+
+
+a = TestBreakpointAPI()
+a.test_requires_auth_key()
